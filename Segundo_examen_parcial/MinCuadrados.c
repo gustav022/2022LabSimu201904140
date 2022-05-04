03c876f7545a5e011b89376eb85447fd74377120
gustav022 <gustav2030manzia@gmail.com>
2022-05-03 18:15:56 -0600

    04MAY

--- Segundo_examen_parcial/MinCuadrados.c
+++ Segundo_examen_parcial/MinCuadrados.c
@@ -11,6 +11,7 @@
 //Librerias
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 //Se declaran e inicializan las variables globales
 float x[]={1,2,3,4,5,6,7,8}, y[]={660.0,671.4,668.0,695.5,717.1,759.2,807.0,859.7};
@@ -25,10 +26,12 @@ float sumaMulti(float datos1[], float datos2[]);
 
 void main(){
     //Se declaran las variables del problema
-    float b, m, r;
+    float b, m, r, anh, ans, resp;
     //Se imprimen los valores de los vectores para que el usuario sepa la informacion
     imprimir(x);
     imprimir(y);
+    //Se grafican los valores de la tabla inicial del problema
+    system("gnuplot GrafParcial1a.gp");
     //Los valores de ecuación lineal, por medio de mínimos cuadrados
     m = (n*sumaMulti(x,y)-(suma(x)*suma(y)))/(n*sumaMulti(x,x)-(suma(x)*suma(x)));
     b = (suma(y)-m*suma(x))/n;
@@ -37,6 +40,16 @@ void main(){
     printf("y = %fx + %f\n",m,b);
     //Imprime coeificiente de determinación el r cuadrado
     printf("Coeficiente de determinación: %f\n",r*r);
+    //Se le pide al usuario que ingrese el año al que quiere saber la producción aproximada de cardamomo
+    printf("Ingrese el año al que quiere saber la aproximación (el año debe de ser después de 2013):\n");
+    scanf("%f",&anh); 
+    printf("La aproximación para el año ingresado es:\n");
+    ans = (anh - 2013);
+    resp = m*ans + b;
+    //Se imprime el valor de la producción de cardamomo en el año ingreasado por el usuario
+    printf("%f \n", resp);
+    //Se genera la gráfica de la aproximación
+    system("gnuplot GrafParcial1b.gp");
 }
 
 //Imprimir los datos del vector que ingrese a la función
